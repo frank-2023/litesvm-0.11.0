738084c55db65e89fe84c7870c8adec97ad8149c
frank-2023 <cye384034@gmail.com>
2026-05-14 14:58:51 +0800

    test 是否被注册

--- crates/litesvm/src/message_processor.rs
+++ crates/litesvm/src/message_processor.rs
@@ -36,6 +36,8 @@ pub(crate) fn process_message<'ix_data>(
             })?;
 
         let mut compute_units_consumed = 0;
+        println!("is_precompile: {:?}",invoke_context.is_precompile(program_id));
+        println!("program_id: {:?}",program_id);
         let result = if invoke_context.is_precompile(program_id) {
             invoke_context.process_precompile(
                 program_id,
